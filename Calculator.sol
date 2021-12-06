20a8c6cb9053bf1978a90c2f0be86bfa58138765
Param Choudhary <69947192+Paramchoudhary@users.noreply.github.com>
2021-12-06 12:49:11 +0530

    Calculator
    add, sub, divide, multiply,modulous

---
+++ Calculator.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.7 ;
+
+contract Calculator{
+uint x;
+uint y;
+    function setInput(uint a, uint b) public {
+        x =a;
+        y =b;
+
+    }
+    function sub() public returns(uint){
+        if(x>y){
+            return x-y;
+        }
+        else{
+            return y-x;
+        }
+    }
+     function divide() public view returns(uint){
+        return x/y;
+    }
+     function multiply() public view returns(uint){
+        return x*y;
+    }
+     function modulus() public view returns(uint){
+        return x%y;
+    }
+}
