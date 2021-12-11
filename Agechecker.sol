b3a5e5c14e9feee2277b359659ace43e8cf9322a
Param Choudhary <69947192+Paramchoudhary@users.noreply.github.com>
2021-12-11 11:39:39 +0530

    updated agechecker

---
+++ Agechecker.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+contract Agechecker {
+  
+   function ageChecker(uint age) public pure returns(string memory){
+   if(age<=13){
+       return "kid";
+   }else if(age<=18){
+       return "boy";
+   }else if(age<= 40){
+       return "Young";
+   }else if(age>=40){
+       return "Old Man";
+   }
+
+
+   }
+
+}
