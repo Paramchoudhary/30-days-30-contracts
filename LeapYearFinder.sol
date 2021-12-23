9f61c33c781e6ecf3ce700ad74c3332f7c2dbae5
Param Choudhary <69947192+Paramchoudhary@users.noreply.github.com>
2021-12-24 01:33:07 +0530

    Create LeapYearFinder.sol

---
+++ LeapYearFinder.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.8.2;
+
+contract LeapYearFinder
+{
+
+
+function leapyear(uint year) public pure returns(bool){
+if( year % 4 == 0){
+  return false;
+}
+  if( year % 100 == 0){
+    return true;
+  }
+  if( year % 400 == 0){
+  return false;
+}
+
+      }   
+}
