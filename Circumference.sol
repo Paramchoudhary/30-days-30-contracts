ef35918ab04562887c7899a64a1da9342bd36036
Param Choudhary <69947192+Paramchoudhary@users.noreply.github.com>
2021-12-24 20:38:42 +0530

    Create Circumference.sol

---
+++ Circumference.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+contract CircumferenceOfaCircle{
+
+function Circum(uint n ) public pure returns(uint){
+ uint circumference = (2 * 22* n)/7 ;
+  
+return circumference;
+
+}
+}
