726aeb1a87bf77b9f84bde0967e9cc66fba3a5b9
Param Choudhary <69947192+Paramchoudhary@users.noreply.github.com>
2022-01-13 21:37:51 +0530

    Update ToDoList.sol

--- ToDoList.sol
+++ ToDoList.sol
@@ -1,25 +1,27 @@
+
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.10;
 
 contract ToDoList {
-uint public taskCount = 0;
-struct Task{
- uint id;
- string content;
- bool completed;
+uint public taskcount =0;
+struct task{
+uint id;
+string content;
+bool completed;
 
 }
-mapping(uint => Task) public tasks;
-
+mapping(uint => task) public tasks;
 constructor() public {
   createTask("My name is Param");
 }
-function createTask(string memory _content) public{
- taskCount++;
-tasks[taskCount] = Task(taskCount, _content,false);
 
+function createtask(string memory _content) public{
+  taskcount++;
+  tasks[taskcount] = task(taskcount,_content,false);
 
 }
+
+
 function toggleCompleted(uint _id) public{
 Task memory _task = tasks[_id];
 _task.completed = !_task.completed;
