d47f149d483fa4f68b3c6101ea020492c66bc16c
Param Choudhary <69947192+Paramchoudhary@users.noreply.github.com>
2022-01-27 22:47:03 +0530

    Update Balance.sol

--- Balance.sol
+++ Balance.sol
@@ -2,13 +2,17 @@
 pragma solidity ^0.8.1;
 
 contract Payable{
+//payable keyword means that the function can process transactions with non-zero Ether value
 address payable public owner;
 constructor () {
+// owner address should be payable
     owner = payable(msg.sender);
 }
+// All ether are deposit in msg.sender Address.
 function deposit() external payable{}
+// check the balance of msg.sender address
 function getBalance() external view returns(uint){
     return address(this).balance;
 }
 }
-//created by Param
+
