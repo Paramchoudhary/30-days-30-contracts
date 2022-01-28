97de76089165d0500eb6a801deeb9a817e787b1f
Param Choudhary <69947192+Paramchoudhary@users.noreply.github.com>
2022-01-28 14:52:13 +0530

    Update ChangeOwner.sol

--- ChangeOwner.sol
+++ ChangeOwner.sol
@@ -3,6 +3,7 @@
 pragma solidity ^0.8.0;
 
 contract AccessRestriction{
+//block.timestamp = time
     uint time_ = block.timestamp;
  address payable public owner  = payable(msg.sender);
  uint public lastOwnerChange = time_;
