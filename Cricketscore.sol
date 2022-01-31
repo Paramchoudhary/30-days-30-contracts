d210e78843c6aac5ce510c1ef03cff3b6663f79d
Param Choudhary <69947192+Paramchoudhary@users.noreply.github.com>
2022-01-31 22:30:00 +0530

    Update Cricketscore.sol

--- Cricketscore.sol
+++ Cricketscore.sol
@@ -5,6 +5,7 @@ contract Cricket{
 uint public player1runs = 0;
 uint public ballcount = 0;
 uint public over;
+//single run
 function single() public{
     
     ballcount++;
@@ -13,12 +14,14 @@ player1runs += 1;
 
 
 }
+//four run
 function four() public{
     ballcount++;
     over = ballcount/6;
 player1runs += 4;
 
 }
+//six run
 function six() public{
     
     ballcount++;
